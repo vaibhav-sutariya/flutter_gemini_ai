c98190699ca4f0057608441c4f184f2166fb0e99
vaibhav-sutariya <98417187+vaibhav-sutariya@users.noreply.github.com>
2024-01-13 21:41:16 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini_bot/mybot.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gemini AI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Gemini AI'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title, textAlign: TextAlign.center),
+      ),
+      body: const ChatBot(),
+    );
+  }
+}
---
+++ lib/mybot.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ChatBot extends StatefulWidget {
+  const ChatBot({super.key});
+
+  @override
+  State<ChatBot> createState() => _ChatBotState();
+}
+
+class _ChatBotState extends State<ChatBot> {
+  ChatUser myself = ChatUser(id: '1', firstName: 'vaibhav');
+  ChatUser bot = ChatUser(id: '2', firstName: 'gemini');
+
+  List<ChatMessage> allMessages = [];
+  List<ChatUser> typing = [];
+
+  final oururl =
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyB6wwW6Yus3as01hM0eOUJw6RtED1Zv0ws';
+
+  final header = {'Content-Type': 'application/json'};
+
+  getData(ChatMessage m) async {
+    typing.add(bot);
+    allMessages.insert(0, m);
+    setState(() {});
+
+    var data = {
+      "contents": [
+        {
+          "parts": [
+            {"text": m.text}
+          ]
+        }
+      ]
+    };
+
+    await http
+        .post(Uri.parse(oururl), headers: header, body: jsonEncode(data))
+        .then((value) {
+      if (value.statusCode == 200) {
+        var result = jsonDecode(value.body);
+        // print(result['candidates'][0]['content']['parts'][0]['text']);
+
+        ChatMessage m1 = ChatMessage(
+            text: result['candidates'][0]['content']['parts'][0]['text'],
+            user: bot,
+            createdAt: DateTime.now());
+
+        allMessages.insert(0, m1);
+      } else {
+        print("error Occured!");
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error Occured!'), ));
+      }
+    }).catchError((e) {});
+
+    typing.remove(bot);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DashChat(
+        inputOptions: InputOptions(autocorrect: true, sendOnEnter: true,),
+        scrollToBottomOptions: ScrollToBottomOptions(disabled: false),
+        typingUsers: typing,
+          currentUser: myself,
+          onSend: (ChatMessage m) {
+            getData(m);
+          },
+          messages: allMessages),
+    );
+  }
+}
